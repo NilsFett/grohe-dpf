b9b026c7157ee4db4cee400f0496f0fac8447dd5
Nils Fett <mail@nils-fett.de>
2019-01-17 11:09:51 +0100

    design

--- src/app/services/config.service.ts
+++ src/app/services/config.service.ts
@@ -2,7 +2,8 @@ import { Injectable, Inject } from '@angular/core';
 
 @Injectable()
 export class ConfigService {
-  public baseURL = 'http://grohe-dpf.localdomain/api/';
+  public baseURL = 'http://groheapi.localdomain/api/';
+  //public baseURL = 'http://grohe2.hoehne-media.de/api/';
 
   constructor(
   ) {
