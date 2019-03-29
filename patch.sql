aff867b932b9bbeea36b17c3af587db8da0d52e3
Nils Fett <nilsfett@Nilss-iMac.fritz.box>
2019-03-29 16:06:44 +0100

    sql patch

--- patch.sql
+++ patch.sql
@@ -60,3 +60,5 @@ ALTER TABLE `t_articles` CHANGE `deleted` `deleted` TINYINT(4) NOT NULL DEFAULT 
 ALTER TABLE `t_display_parts` CHANGE `open_format` `open_format` VARCHAR(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL;
 ALTER TABLE `t_display_parts` CHANGE `stock` `stock` INT(11) NULL;
 INSERT INTO `actions` (`id`, `enviroment_id`, `name`, `url`) VALUES (NULL, '1', 'productTree', '/productTree');
+INSERT INTO `actions` (`id`, `enviroment_id`, `name`, `url`) VALUES (NULL, '1', 'loadDisplasPartsByDisplayId', '/loadDisplasPartsByDisplayId');
+INSERT INTO `actions` (`id`, `enviroment_id`, `name`, `url`) VALUES (NULL, '1', 'saveDisplayAndPartList', '/saveDisplayAndPartList');
