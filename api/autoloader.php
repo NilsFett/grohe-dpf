93e930a6f8c632d07d878cb78c90f34e6ce1a7f9
Nils Fett <mail@nils-fett.de>
2019-05-20 13:25:07 +0200

    order export

--- api/autoloader.php
+++ api/autoloader.php
@@ -13,8 +13,28 @@ INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PA
 IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
 TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
+/*
 function __autoload($sClassName) {
 
+	if(! class_exists($sClassName)){
+		$oConfig = cConfig::getInstance();
+
+
+		if(is_file($oConfig->get('basepath').'classes/'.$sClassName.'.php'))
+    		require_once $oConfig->get('basepath').'classes/'.$sClassName.'.php';
+		elseif(is_file($oConfig->get('basepath').'models/'.$sClassName.'.php'))
+    		require_once $oConfig->get('basepath').'models/'.$sClassName.'.php';
+		elseif(is_file($oConfig->get('basepath').'controllers/'.$sClassName.'.php'))
+    		require_once $oConfig->get('basepath').'controllers/'.$sClassName.'.php';
+		elseif(is_file($oConfig->get('basepath').'views/'.$sClassName.'.php'))
+    		require_once $oConfig->get('basepath').'views/'.$sClassName.'.php';
+    	else
+    		throw new Exception('Class '.$sClassName.' not found !');
+	}
+}
+*/
+
+spl_autoload_register(function ($sClassName) {
 	if(! class_exists($sClassName)){
 		$oConfig = cConfig::getInstance();
 /*
@@ -32,7 +52,28 @@ function __autoload($sClassName) {
     		require_once $oConfig->get('basepath').'controllers/'.$sClassName.'.php';
 		elseif(is_file($oConfig->get('basepath').'views/'.$sClassName.'.php'))
     		require_once $oConfig->get('basepath').'views/'.$sClassName.'.php';
-    	else
+    			if(! class_exists($sClassName)){
+		$oConfig = cConfig::getInstance();		
+		$parts = explode('\\', $sClassName);
+		$withoutNamespace = end($parts);		
+		$sClassNamePath = str_replace('\\','/',$sClassName);		
+
+		if(is_file($oConfig->get('basepath').'libs/'.$sClassNamePath.'.php')){
+    		require_once $oConfig->get('basepath').'libs/'.$sClassNamePath.'.php';
+		}
+		elseif(is_file($oConfig->get('basepath').'libs/'.$sClassNamePath.'/'.$withoutNamespace.'.php')){
+    		require_once $oConfig->get('basepath').'libs/'.$sClassNamePath.'/'.$withoutNamespace.'.php';
+		}
+		elseif(is_file($oConfig->get('basepath').'libs/simple-cache/'.$withoutNamespace.'.php')){
+    		require_once $oConfig->get('basepath').'libs/simple-cache/'.$withoutNamespace.'.php';
+		}
+		elseif(is_file($oConfig->get('basepath').'libs/PhpOffice/PhpSpreadsheet/Writer/'.$sClassName.'.php')){
+    		require_once $oConfig->get('basepath').'libs/PhpOffice/PhpSpreadsheet/Writer/'.$sClassName.'.php';
+		}
+    	else{
     		throw new Exception('Class '.$sClassName.' not found !');
+		}
 	}
-}
+
+	}
+});
--- src/app/classes/DisplaysPart.ts
+++ src/app/classes/DisplaysPart.ts
@@ -2,7 +2,7 @@ export class DisplaysPart {
   public id:number;
   public title:string;
   public articlenr:number;
-  public length:string;
+  public length:string = '';
   public width:string;
   public height:string;
   public stock:number;
