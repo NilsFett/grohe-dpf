57a93595119f47459236e0088d87b1e8bbea3f4b
euphoriatek2010 <euphoriatek2010@gmail.com>
2023-08-05 17:24:32 +0530

    chnage base url for api

--- src/app/services/config.service.ts
+++ src/app/services/config.service.ts
@@ -3,9 +3,9 @@ import { Injectable, Inject } from '@angular/core';
 @Injectable()
 export class ConfigService {
 
-  //public baseURL = 'http://groheapi.localdomain/api/';
+  public baseURL = 'http://groheapi.localdomain/api/';
   // public baseURL = 'http://example.local/api/';
-  public baseURL = 'https://grohe.local/api/'; 
+  // public baseURL = 'https://grohe.local/api/'; 
   static baseURL: any;
 
 
